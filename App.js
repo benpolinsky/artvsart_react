0e3619557966616c5cbcdd871b0883c3a7ff3f31
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-08-18 15:35:09 -0400

    transfer to main comp

--- App.js
+++ App.js
@@ -1,9 +1,53 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return <div>Hello World, My Name is Ben Polinsky</div>
+class App extends React.Component{
+  constructor(){
+    super();
+    
+    this.state = {
+      txt: "this is the txt state",
+      cat: 0
+    }
+    
+    this.update = this.update.bind(this);
+  }
+  
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+  
+  render(){
+    let text = this.state.txt;
+    let category_id = "category_" + this.state.category_id;
+    
+    return (
+      <div>
+        <Widget txt={this.state.txt} category_id={this.state.category_id} update={this.update} />
+        <Widget txt={this.state.txt} category_id={this.state.category_id} update={this.update} />
+        <Widget txt={this.state.txt} category_id={this.state.category_id} update={this.update} />
+        <Widget txt={this.state.txt} category_id={this.state.category_id} update={this.update} />
+      </div>
+    )
   }
 }
+
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   category_id: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "This is the default!"
+// }
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type='text' onChange={props.update} />
+      <h1 id={props.category_id}>{props.txt}</h1> 
+    </div>
+  ) 
+}
+
 
 export default App
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App txt="Hello World, I'm a prop value" category_id={12} />, 
+  document.getElementById('app')
+);
