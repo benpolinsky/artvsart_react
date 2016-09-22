6959b666b2be9504eeceafb70e3d38b48bd8ecf6
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-09-22 09:48:08 -0400

    plan app state

---
+++ app_state.js
@@ -0,0 +1,34 @@
+const AppState = {
+  info: {
+    total_art: 0,
+    total_art_judged: 0,
+    total_competitions: 0
+  },
+  
+  competition: {
+    winning_art: {
+      name: "",
+      creator: ""
+    },
+    
+    losing_art: {
+      name: "",
+      creator: ""
+    }, 
+    
+    art: {
+      name: "",
+      creator: ""
+    },
+    
+    challenger: {
+      name: "",
+      creator: ""
+    }
+  },
+  
+  current_art: {
+    name: "",
+    creator: ""
+  }
+}
